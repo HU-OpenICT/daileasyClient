99f0ce53d52d5ddc81b597687900070574f9b3e0
Zoë <zoehectors@hotmail.com>
2020-10-28 10:43:07 +0100

    toevoeging starttijd

---
+++ js/fetch.js
@@ -0,0 +1,31 @@
+var today = new Date();
+var start_time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+
+function send_checkin() {
+
+    let start_time = start_time;
+    let date = document.getElementById('date').value;
+    let team = document.querySelector('input[name="team"]:checked').value;
+    let feeling = document.querySelector('input[name="feeling"]:checked').value;
+    let why_feeling = document.getElementById('why_feeling').value;
+    let did = document.getElementById('did').value;
+    let learned = document.getElementById('learned').value;
+    let todo = document.getElementById('todo').value;
+    let question = document.getElementById('question').value;
+
+
+    let data = { 'start_time': start_time, 'date': date, 'team': team, 'feeling': feeling, 'why_feeling': why_feeling, 'did': did, 'learned': learned, 'todo': todo, 'question': question };
+
+    fetch('http://127.0.0.1:5000/api/checkins', {
+            method: 'POST',
+            headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
+            body: JSON.stringify(data)
+        })
+        .then((response) => {
+            return response.json();
+        })
+        .then((result) => {
+            alert(result['succes']);
+        });
+
+}
